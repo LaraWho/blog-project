25ecd8794ce7f70bcfdc43e8d8aa25cbbe19bb7a
Lara Potjewyd <larapotjewyd@gmail.com>
2019-04-04 17:37:54 -0600

    Styled inputs and button on addArticle page

--- src/components/AddArticle.js
+++ src/components/AddArticle.js
@@ -3,26 +3,22 @@ import 'react-quill/dist/quill.snow.css';
 import axios from 'axios';
 import { withState } from '../MyState';
 import styled from 'styled-components';
+import Login from './Login';
 const ReactQuill = require('react-quill');
 
 const Editor = styled.div`
 
 `
-
 const Input = styled.input`
-  :nth-of-type(1) {
-    margin-top: 1em;
-  }
-  
+  margin: 0.5em;
 `
-
 const AddButton = styled.h2`
   cursor: pointer;
   width: 60vw;
   margin: auto;
   padding: 0.25em;
   background-color: #d8d8d8;
-  margin-top: 0.5em;
+  margin-top: 0.75em;
   transition: all .5s ease;
   text-align: left;
   :hover {
@@ -30,7 +26,17 @@ const AddButton = styled.h2`
     color: #FFF;
   }
 `
-
+const InputText = styled.h2`
+  text-align: left;
+  width: 80vw;
+  margin: auto;
+  :nth-of-type(1) {
+    margin-top: 1em;
+  }
+  @media (min-width: 600px) {
+    width: 60vw;
+  }
+`
 class AddArticle extends Component {
   constructor(props) {
     super(props);
@@ -81,6 +87,8 @@ class AddArticle extends Component {
     const { imageURL, link, title } = this.state
     return (
       <Editor>
+      {this.props.isLoggedIn ?
+      <Editor>      
         <div className="text-editor">
           <ReactQuill theme="snow"
                       onChange={this.handleChange}
@@ -88,11 +96,21 @@ class AddArticle extends Component {
                       formats={this.formats}>
           </ReactQuill>
         </div>
-        <Input type="text" placeholder="title" name="title" value={title} onChange={this.handleInputChange}/>
-        <Input type="text" placeholder="imageurl" name="imageURL" value={imageURL} onChange={this.handleInputChange}/>
-        <Input type="text" placeholder="email" name="link" value={link} onChange={this.handleInputChange}/>
+        <InputText>Title</InputText>
+        <Input type="text" name="title" value={title} onChange={this.handleInputChange}/>
+        <InputText>Image URL Only</InputText>
+        <Input type="text" name="imageURL" value={imageURL} onChange={this.handleInputChange}/>
+        <InputText>Publication Link</InputText>
+        <Input type="text" name="link" value={link} onChange={this.handleInputChange}/>
         <AddButton onClick={() => this.addArticle()}>add</AddButton>
       </Editor>
+      :
+      <Editor>
+        <Login history={this.props.history}/>
+      </Editor>
+      }
+      </Editor>
+
     );
   }
 }

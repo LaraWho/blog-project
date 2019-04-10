609574d82679f2048341caac5d0dc401bdfb2d06
Lara Potjewyd <larapotjewyd@gmail.com>
2019-04-10 13:36:01 -0600

    fixed env var

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const expressJWT = require("express-jwt");
-const port = process.env.SERVER_PORT;
+const port = process.env.PORT;
 const mongoose = require("mongoose");
 const path = require("path");
 

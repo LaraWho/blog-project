7ae15ca246fff7efdc88de0b9548e79fb4506a1d
Lara Potjewyd <larapotjewyd@gmail.com>
2019-04-10 19:00:13 -0600

    fixed typo

--- src/components/SingleArticle.js
+++ src/components/SingleArticle.js
@@ -20,7 +20,7 @@ const ArticleHeader = styled.h1`
     font-size: 3em;
   }
 `;
-const ArticleImage = styled.im`
+const ArticleImage = styled.img`
   width: 100vw;
   margin: 1em;
   @media (min-width: 600px) {
